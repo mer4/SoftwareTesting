13e62b19624547e3282e4dcfce5d9451018b3f82
kbp46 <48021326+kbp46@users.noreply.github.com>
2019-03-04 18:31:08 -0500

    Add files via upload

--- Project/ClassProject/SpeechTextApp/public/appRoutes.js
+++ Project/ClassProject/SpeechTextApp/public/appRoutes.js
@@ -3,10 +3,28 @@ angular
     .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
     $stateProvider.state({
+        name: 'home',
+        url: '/home',
+        templateUrl: 'public/components/templates/home.view.html',
+        controller: 'HomeController'
+    });
+    $stateProvider.state({
         name: 'login',
-        url: '/register',
-        templateUrl: 'public/components/templates/login.template.html',
+        url: '/login',
+        templateUrl: 'public/components/templates/login.view.html',
         controller: 'LoginController'
+    });
+    $stateProvider.state({
+        name: 'register',
+        url: '/register',
+        templateUrl: 'public/components/templates/register.view.html',
+        controller: 'RegisterController'
+    });
+    $stateProvider.state({
+        name: 'dashboard',
+        url: '/d',
+        templateUrl: 'public/components/templates/dashboard.html',
+        controller: 'DashboardController'
     });
 
     $urlRouterProvider.otherwise('/');
