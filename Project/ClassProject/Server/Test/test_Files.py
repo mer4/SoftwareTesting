86ab71e4dc308d6cdd465a4af40c148e22394422
Cesar Salazar <cesarl_88@hotmail.com>
2019-03-06 07:42:52 -0500

    Test file setup

---
+++ Project/ClassProject/Server/Test/test_Files.py
@@ -0,0 +1,15 @@
+import pytest
+import unittest
+from django.contrib.auth.models import User
+#from SpeechToText.models import File, FileType
+
+#@pytest.mark.django_db
+#class TestFiles:
+ #   def setup(self):
+
+        # user_obj = User.objects.create_user(username = "cesar", password="1234", email="cls33@njit.edu")
+        # user_obj.save()
+
+        # file_obj_Audio = File.objects.crete_file(Name='Audio File', Type = 0, User = user_obj)
+        # file_obj_Video = File.objects.crete_file(Name='Video File', Type = 1, User = user_obj)
+        # file_obj_Mic = File.objects.crete_file(Name='Microphone File', Type = 2, User = user_obj)
